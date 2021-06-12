3b22cdfcc0d45cdba3c0fdb1597ce94b59434ee8
Lucky56 <55949451+Lucky-56@users.noreply.github.com>
2021-06-12 10:51:14 +0200

    replay fixies

--- source/Replay.hx
+++ source/Replay.hx
@@ -32,7 +32,7 @@ class Replay
     {
         this.path = path;
         replay = {
-            songName: "Tutorial", 
+            songName: "No song found", 
             songDiff: 1,
 			noteSpeed: 1.5,
 			isDownscroll: false,
